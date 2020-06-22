a149f367a637a0caf4defc0b8ebbee1c3fa579a6
Carsten Klaffke <mail@carsten-klaffke.de>
2020-06-22 22:20:21 +0200

    added Example

---
+++ Example/SendIntentExample/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,41 @@
+package io.ionic.starter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.ValueCallback;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+import de.mindlib.sendIntent.SendIntent;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initializes the Bridge
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+            // Additional plugins you've installed go here
+            add(SendIntent.class);
+        }});
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        String action = intent.getAction();
+        String type = intent.getType();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            bridge.getActivity().setIntent(intent);
+            bridge.eval("window.dispatchEvent(new Event('sendIntentReceived'))", new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String s) {
+                }
+            });
+        }
+    }
+
+}
---
+++ Example/SendIntentExample/src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+import {Plugins} from '@capacitor/core';
+
+const Home: React.FC = () => {
+
+    function checkIntent() {
+        Plugins.SendIntent.checkSendIntentReceived().then((result: any) => (async function (result: any) {
+            alert(result.url)
+            alert(result.text)
+            alert(result.image)
+        })(result)).catch((err:any) => console.log(err))
+    }
+
+    window.addEventListener("sendIntentReceived", () => {
+        checkIntent();
+    })
+
+    checkIntent();
+
+    return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
