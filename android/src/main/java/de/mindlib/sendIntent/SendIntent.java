5bf5848a27ff8612b17d7b8c1c51cd9d7ecd74e7
Carsten Klaffke <mail@carsten-klaffke.de>
2021-08-02 22:52:06 +0200

    removed unused imports

--- android/src/main/java/de/mindlib/sendIntent/SendIntent.java
+++ android/src/main/java/de/mindlib/sendIntent/SendIntent.java
@@ -11,17 +11,9 @@ import com.getcapacitor.PluginMethod;
 
 import org.apache.commons.io.IOUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @NativePlugin()
 public class SendIntent extends Plugin {
