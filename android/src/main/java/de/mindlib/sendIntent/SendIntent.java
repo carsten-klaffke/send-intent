ec0883312eba1c6cb18e624a225c7af37b555751
Carsten Klaffke <mail@carsten-klaffke.de>
2021-11-09 15:30:04 +0100

    Multifile support

--- android/src/main/java/de/mindlib/sendIntent/SendIntent.java
+++ android/src/main/java/de/mindlib/sendIntent/SendIntent.java
@@ -1,19 +1,19 @@
 package de.mindlib.sendIntent;
 
-import android.content.ContentProvider;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.OpenableColumns;
-import android.widget.TextView;
 
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
-import org.apache.commons.io.FilenameUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 @CapacitorPlugin()
 public class SendIntent extends Plugin {
@@ -26,30 +26,37 @@ public class SendIntent extends Plugin {
             String type = intent.getType();
             intent.setAction("intent handled");
             if (Intent.ACTION_SEND.equals(action) && type != null) {
-                JSObject ret = new JSObject();
-                String title = intent.getStringExtra(Intent.EXTRA_TEXT);
-                Uri uri = intent.getClipData().getItemAt(0).getUri();
-
-                if(title == null && uri !=null)
-                    title = readFileName(uri);
-
-                ret.put("title", title);
-                ret.put("description", null);
-                ret.put("type", type);
-                ret.put("url", uri.toString());
-                call.resolve(ret);
+                call.resolve(readItemAt(intent, type, 0));
             } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
-                if (type.startsWith("image/")) {
+                JSObject ret = readItemAt(intent, type, 0);
+                List additionalItems = new ArrayList<JSObject>();
 
+                for (int index = 1; index < intent.getClipData().getItemCount(); index++) {
+                    additionalItems.add(readItemAt(intent, type, index));
                 }
-            } else {
-                // Handle other intents, such as being started from the home screen
+                ret.put("additionalItems", new JSArray(additionalItems));
+                call.resolve(ret);
             }
         }
         call.reject("No processing needed");
     }
 
-    public String readFileName(Uri uri){
+    private JSObject readItemAt(Intent intent, String type, int index) {
+        JSObject ret = new JSObject();
+        String title = intent.getStringExtra(Intent.EXTRA_TEXT);
+        Uri uri = intent.getClipData().getItemAt(index).getUri();
+
+        if (title == null && uri != null)
+            title = readFileName(uri);
+
+        ret.put("title", title);
+        ret.put("description", null);
+        ret.put("type", type);
+        ret.put("url", uri.toString());
+        return ret;
+    }
+
+    public String readFileName(Uri uri) {
         Cursor returnCursor =
                 getContext().getContentResolver().query(uri, null, null, null, null);
         /*
